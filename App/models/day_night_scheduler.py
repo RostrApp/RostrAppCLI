671036f8bebf92f8b17f3ba12d606b46bcdbbdf0
CidWB <cidneyleigh1@gmail.com>
2025-11-09 17:27:47 -0400

    rebased to original commit

--- App/models/day_night_scheduler.py
+++
@@ -1,9 +0,0 @@
-from App.controllers.user import get_all_users_by_role, get_all_shifts
-from App.models.scheduling_strategy import SchedulingStrategy
-
-class DayNightScheduler(SchedulingStrategy):
-
-    def schedule_shift(self):
-        # return Schedule object -> create_day_night_schedule(self.all_staff, self.shifts)
-        # this method will be in Schedule controller
-        pass
