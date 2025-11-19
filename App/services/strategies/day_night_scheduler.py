224879b68209046bea913e5006727d80a44d31cb
CidWB <cidneyleigh1@gmail.com>
2025-11-19 19:25:25 -0400

    Added Scheduling services for task #8

---
+++ App/services/strategies/day_night_scheduler.py
@@ -0,0 +1,9 @@
+from App.controllers.user import get_all_users_by_role, get_all_shifts
+from App.services.scheduling_strategy import SchedulingStrategy 
+
+class DayNightScheduler(SchedulingStrategy):
+
+    def fill_schedule(self):
+        # return Schedule object -> create_day_night_schedule(self.all_staff, self.shifts)
+        # this method will be in Schedule controller
+        pass
