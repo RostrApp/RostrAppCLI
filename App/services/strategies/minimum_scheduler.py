39c181c2a62f2e1428e7cd1854a9fa01dd361b8a
CidWB <cidneyleigh1@gmail.com>
2025-11-19 19:48:22 -0400

    Fixed methods for scheduling strategies

--- App/services/strategies/minimum_scheduler.py
+++ App/services/strategies/minimum_scheduler.py
@@ -1,9 +1,11 @@
+from App.models.staff import Staff
+from App.models.schedule import Schedule
 from App.controllers.user import get_all_users_by_role, get_all_shifts
 from App.services.scheduling_strategy import SchedulingStrategy 
 
 class MinimumScheduler(SchedulingStrategy):
 
-    def fill_schedule(self):
+    def fill_schedule(self, staff, schedule):
         # return Schedule object -> create_minimum_schedule(self.all_staff, self.shifts)
         # this method will be in Schedule controller
         pass
