e123d86b520af75d5e6d118117e9c24e4151c010
CidWB <cidneyleigh1@gmail.com>
2025-11-19 19:19:28 -0400

    test

--- App/models/scheduling_strategy.py
+++ App/models/scheduling_strategy.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from App.controllers.user import get_all_users_by_role, get_all_shifts
 
 class SchedulingStrategy(ABC):
-    
+    # hello
     # gets updated list from db upon initialization
     def __init__(self):
         super().__init__()
