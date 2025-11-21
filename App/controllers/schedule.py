aaacc6fe57a677215afb55124002ccf50c5708da
Shania <shaniasiew14@gmail.com>
2025-11-21 05:47:03 -0400

    Tested Cli special feature

--- App/controllers/schedule.py
+++ App/controllers/schedule.py
@@ -1,7 +1,8 @@
 from App.controllers.user import get_all_users_by_role, get_all_shifts
 from App.models.schedule import Schedule
-from App.models.even_scheduler import EvenScheduler, MinimumScheduler, DayNightScheduler
-
+from App.models.even_scheduler import EvenScheduler
+from App.models.minimum_scheduler import MinimumScheduler
+from App.models.day_night_scheduler import DayNightScheduler
 
 def create_even_schedule(all_staff, shifts, creator_id):
     scheduler = EvenScheduler()
