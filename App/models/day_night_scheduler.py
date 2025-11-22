af0c5f8cda40041d942af77488cd0eeaba8d45d7
Shania <shaniasiew14@gmail.com>
2025-11-22 02:39:35 -0400

    #9 Develop Strategy Algorithms

--- App/models/day_night_scheduler.py
+++ App/models/day_night_scheduler.py
@@ -1,9 +1,26 @@
-from App.controllers.user import get_all_users_by_role, get_all_shifts
+# Task 9: DayNightScheduler class implementation
 from App.models.scheduling_strategy import SchedulingStrategy
+from App.models.schedule import Schedule
 
 class DayNightScheduler(SchedulingStrategy):
 
-    def schedule_shift(self):
-        # return Schedule object -> create_day_night_schedule(self.all_staff, self.shifts)
-        # this method will be in Schedule controller
-        pass
+    def schedule_shift(self, staff_list, shift_list, creator_id):
+        schedule = Schedule(
+            name="Day Night Schedule",
+            created_by=creator_id
+        )
+
+        if len(staff_list) < 2:
+            raise Exception("Need at least 2 staff for day/night scheduling")
+
+        day_staff = staff_list[0]
+        night_staff = staff_list[1]
+
+        for shift in shift_list:
+            if shift.start_time.hour < 18:
+                shift.staff_id = day_staff.id
+            else:
+                shift.staff_id = night_staff.id
+            schedule.shifts.append(shift)
+
+        return schedule
