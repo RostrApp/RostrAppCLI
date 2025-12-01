ed8ac9f1f7d9851c0f59c9f219182798e0c70abe
CidWB <cidneyleigh1@gmail.com>
2025-11-30 21:26:19 -0400

    Changed relative imports to absolute ones

--- App/controllers/schedule.py
+++ App/controllers/schedule.py
@@ -1,6 +1,6 @@
 from App.controllers.user import get_all_users_by_role, get_all_shifts
 from App.models.schedule import Schedule
-
+# need to implement scheduling logic
 
 def create_even_schedule(all_staff, shifts):
     # write function
